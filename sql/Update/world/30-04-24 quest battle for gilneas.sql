36e99b3a05affb79d3a004e2482452303d31901b
RëXol <jorgeluisramirezlorenzo@gmail.com>
2024-05-01 22:18:05 -0400

    Update 30-04-24 quest battle for gilneas.sql

--- sql/Update/world/30-04-24 quest battle for gilneas.sql
+++ sql/Update/world/30-04-24 quest battle for gilneas.sql
@@ -10,7 +10,7 @@ DELETE FROM `creature`  WHERE `guid` = 186494;
 DELETE FROM `creature_text`  WHERE `entry` = 38218;
 DELETE FROM `creature_text`  WHERE `entry` = 38331;
 
-UPDATE `creature_template` SET `AIName`='' `ScriptName`='npc_krennan_aranas' WHERE (`entry`='38553');
+UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_krennan_aranas' WHERE (`entry`='38553');
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES('38470','0','0','0','0','0','100','1','10000','10000','0','0','1','0','1','0','0','0','0','1','0','0','0','0','0','0','0','King Genn Greymane - In Combat - Say Line 0');
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES('38426','0','0','0','54','0','100','0','0','0','0','0','80','3842600','2','0','0','0','0','1','0','0','0','0','0','0','0','Lorna Crowley - On Just Summoned - Run Script');
