2072b33f1f4c3b8cc1bcd6d058c7bd8cfeb1521b
RëXol <jorgeluisramirezlorenzo@gmail.com>
2024-05-02 16:50:19 -0400

    Create 02-05-24 auras duplicadas.sql

---
+++ sql/Update/world/02-05-24 auras duplicadas.sql
@@ -0,0 +1,8 @@
+UPDATE `creature_template_addon` SET `entry`='35505', `auras`='' WHERE (`entry`='35505');
+UPDATE `creature_template_addon` SET `entry`='44675', `auras`='' WHERE (`entry`='44675');
+UPDATE `creature_template_addon` SET `entry`='44671', `auras`='' WHERE (`entry`='44671');
+UPDATE `creature_template_addon` SET `entry`='44685', `auras`='' WHERE (`entry`='44685');
+UPDATE `creature_template_addon` SET `entry`='44673', `auras`='' WHERE (`entry`='44673');
+UPDATE creature_template_addon` SET `entry`='45038', `auras`='' WHERE (`entry`='45038');
+
+UPDATE `creature_addon` SET `guid`='216779', `auras`='' WHERE (`guid`='216779');
