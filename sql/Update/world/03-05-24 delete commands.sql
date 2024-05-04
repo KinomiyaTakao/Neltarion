6c1e3805c3488626a5bbf134e0ae273c7311e2ed
RëXol <jorgeluisramirezlorenzo@gmail.com>
2024-05-03 20:45:26 -0400

    Create 03-05-24 delete commands.sql

---
+++ sql/Update/world/03-05-24 delete commands.sql
@@ -0,0 +1,5 @@
+DELETE FROM `command` WHERE (`name`='spectator');
+DELETE FROM `command` WHERE (`name`='spectate');
+DELETE FROM `command` WHERE (`name`='pickpocketing');
+DELETE FROM `command` WHERE (`name`='creature_onkill_reputation');
+
